04670264b8ee5c72c133eb56f8e17d000c59cfe7
EmeteZ <mtmacedo17@gmail.com>
2025-04-04 18:36:19 -0300

    Start DexAI

---
+++ dex-ai/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-red">
+      <h1 className="text-2xl font-bold">Testando Tailwind no Next.js!</h1>
+    </div>
+  );
+}
---
+++ dex-ai/src/app/pokedex/page.tsx
@@ -0,0 +1,7 @@
+export default function Pokedex() {
+    return (
+      <div className="bg-red">
+        <h1 className="text-2xl font-bold">Marcha papai</h1>
+      </div>
+    );
+  }
